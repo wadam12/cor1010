f3d4d2afb03a3c1f706340cb97169fb1fc921055
wadam12 <163966631+wadam12@users.noreply.github.com>
2024-03-19 13:44:29 +0900

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+for i in range(10) :
+    print("*", end=" ")
+for row in range(10) :
+    print("*",end=" ")
+print()
+for row in range(5) :
+    print("*",end=" ")
+print()
+for row in range(20) :
+    print("*",end=" ")
